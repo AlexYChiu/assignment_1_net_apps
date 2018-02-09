ce99b2a8a6fd15d2c07a490e97b0ad1218181484
Chris Ruppelt <cruppelt@vt.edu>
2018-02-08 22:52:05 -0500

    Actually parses questions now... :)

--- client/my_stream_listener.py
+++ client/my_stream_listener.py
@@ -16,10 +16,22 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         """Prints incoming statuses with hashtag ommitted"""
+        tweet_words = status.text.split()
+        print(tweet_words)
+        question_words = [str(word) for word in tweet_words if word not in self.hashtags]
+        question = ' '.join(word for word in question_words)
+        try:
+            print('Question: ' + str(question.encode('utf8')))                              # this sometimes throws an OSError on Windows... microsoft can't hang.
+        except Exception as ex:                                                             # https://github.com/Microsoft/vscode/issues/39149#issuecomment-347260954 
+            print(ex)  
+
+
+        '''
         tweet_words = status.text.encode('utf8').split()
         question_words = [word for word in tweet_words if word not in self.hashtags]
         question = ' '.join(word.decode('utf-8') for word in question_words)
         try:
             print('Question: ' + str(question.encode('utf8')))                              # this sometimes throws an OSError on Windows... microsoft can't hang.
         except Exception as ex:                                                             # https://github.com/Microsoft/vscode/issues/39149#issuecomment-347260954 
-            print(ex)                                                                       # OSError: raw write() returned invalid length 300 (should have been between 0 and 150)                 
+            print(ex)                                                                       # OSError: raw write() returned invalid length 300 (should have been between 0 and 150)      
+        '''           
