ace5628d2c40db8a4f144ffd164807de6eb90558
Chris Ruppelt <cruppelt@vt.edu>
2018-02-08 19:03:45 -0500

    Testing twitter api

--- client/client.py
+++ client/client.py
@@ -8,6 +8,8 @@
 
 import sys
 import argparse
+import tweepy
+import os
 
 parser = argparse.ArgumentParser(description='Prossesses arguments for client.')
 parser.add_argument('-s', help='Set the server ip address.')
@@ -30,6 +32,18 @@ if args.t == None:
     print('Please set hastag with the -t flag.')
     sys.exit(1)
 
-print(args)
+consumer_token = 	os.environ['CONSUMER_TOKEN']
+consumer_secret =   os.environ['CONSUMER_SECRET']
+access_key =        os.environ['ACCESS_KEY']
+access_secret =     os.environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+
+api = tweepy.API(auth)
+
+user = 'theasianchris1'
+api.get_user(user)
+
 
 print('Finished')
