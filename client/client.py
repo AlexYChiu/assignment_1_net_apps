b339f9e17f4b6af8beff48f6cb23c119edd86f4e
Chris Ruppelt <cruppelt@vt.edu>
2018-02-08 19:05:29 -0500

    WIP

--- client/client.py
+++ client/client.py
@@ -32,10 +32,10 @@ if args.t == None:
     print('Please set hastag with the -t flag.')
     sys.exit(1)
 
-consumer_token = 	os.environ['CONSUMER_TOKEN']
-consumer_secret =   os.environ['CONSUMER_SECRET']
-access_key =        os.environ['ACCESS_KEY']
-access_secret =     os.environ['ACCESS_SECRET']
+consumer_token =        os.environ['CONSUMER_TOKEN']
+consumer_secret =       os.environ['CONSUMER_SECRET']
+access_key =            os.environ['ACCESS_KEY']
+access_secret =         os.environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 auth.set_access_token(access_key, access_secret)
